7612dbfdc9dda48a404b72b2072fd7fd28cd4100
gerardb83 <gbreitenbeck@gmail.com>
2019-07-10 09:26:55 -0400

    extended challenges w/save

--- src/Movie.java
+++ src/Movie.java
@@ -1,40 +1,40 @@
 
 public class Movie {
-	
+
 	private String title;
 	private String category;
-	
+
 	public Movie() {
 	}
-	
+
 	public Movie(String title, String category) {
 		this.title = title;
 		this.category = category.toLowerCase();
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public String getCategory() {
 		return category;
 	}
-	
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
-	
+
 	public void setTitleAndCategory(String title, String category) {
 		this.title = title;
 		this.category = category;
 	}
 
 	public String toString() {
-		return ("Title: " + title + "; Category: " + category); 
+		return ("Title: " + title + "; Category: " + category);
 	}
 
 	public static Movie getMovie(int index) {
@@ -243,5 +243,4 @@ public class Movie {
 			return new Movie("NO SUCH MOVIE", "");
 		}
 	}
-}	
-
+}
--- src/MovieApp.java
+++ src/MovieApp.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class MovieApp {
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		boolean checkout = true;
-		
-		ArrayList <Movie> movieList = new ArrayList<>();
-		
+
+		ArrayList<Movie> movieList = new ArrayList<>();
+
 		movieList.add(new Movie("The Aristocats", "animated"));
 		movieList.add(new Movie("Lawrence of Arabia", "drama"));
 		movieList.add(new Movie("The Shining", "horror"));
@@ -18,20 +18,20 @@ public class MovieApp {
 		movieList.add(new Movie("Pulp Fiction", "drama"));
 		movieList.add(new Movie("Balto", "animated"));
 		movieList.add(new Movie("Scanners", "scifi"));
-		
+
 		ArrayList<String> horror = new ArrayList<>();
 		ArrayList<String> drama = new ArrayList<>();
 		ArrayList<String> scifi = new ArrayList<>();
 		ArrayList<String> musical = new ArrayList<>();
 		ArrayList<String> animated = new ArrayList<>();
 		ArrayList<String> comedy = new ArrayList<>();
-		
+
 		for (int i = 1; i <= 100; i++) {
 			Movie temp = Movie.getMovie(i);
 			String category = temp.getCategory();
 			String title = temp.getTitle();
 			if (category.equals("horror")) {
-			horror.add(title);
+				horror.add(title);
 			} else if (category.equals("drama")) {
 				drama.add(title);
 			} else if (category.equals("scifi")) {
@@ -50,7 +50,7 @@ public class MovieApp {
 		Collections.sort(musical);
 		Collections.sort(animated);
 		Collections.sort(comedy);
-		
+
 		Set<String> categories = new HashSet<>();
 		for (int i = 1; i <= 100; i++) {
 			Movie temp = Movie.getMovie(i);
@@ -60,50 +60,48 @@ public class MovieApp {
 		ArrayList<String> catArray = new ArrayList<>();
 		catArray.addAll(categories);
 		Collections.sort(catArray);
-		
-		for(int i = 0; i < catArray.size(); i++) {
+
+		for (int i = 0; i < catArray.size(); i++) {
 			System.out.println(i + 1 + ". " + catArray.get(i));
 		}
 		System.out.println("\nWelcome to the Movie List Application!\n");
 		System.out.println("There are 100 movies in this list.");
-		
+
 		do {
-		int userInput = ValidationMethods.getRangeInt(scan, "What category are you interested in? ", 1, 6, "Enter a number between 1 and 6");
-		String category = MovieMethods.switchCategory(userInput);
-		///HERE
-		if (category.equals("horror")) {
-			for (String each : horror) {
-			System.out.println(each);
+			int userInput = ValidationMethods.getRangeInt(scan, "What category are you interested in? ", 1, 6,
+					"Enter a number between 1 and 6");
+			String category = MovieMethods.switchCategory(userInput);
+			/// HERE
+			if (category.equals("horror")) {
+				for (String each : horror) {
+					System.out.println(each);
+				}
+			} else if (category.equals("musical")) {
+				for (String each : musical) {
+					System.out.println(each);
+				}
+			} else if (category.equals("animated")) {
+				for (String each : animated) {
+					System.out.println(each);
+				}
+			} else if (category.equals("drama")) {
+				for (String each : drama) {
+					System.out.println(each);
+				}
+			} else if (category.equals("scifi")) {
+				for (String each : scifi) {
+					System.out.println(each);
+				}
+			} else if (category.equals("comedy")) {
+				for (String each : comedy) {
+					System.out.println(each);
+				}
 			}
-		} else if (category.equals("musical")) {
-			for (String each : musical) {
-				System.out.println(each);
-				}
-		} else if (category.equals("animated")) {
-			for (String each : animated) {
-				System.out.println(each);
-				}
-		} else if (category.equals("drama")) {
-			for (String each : drama) {
-				System.out.println(each);
-				}
-		} else if (category.equals("scifi")) {
-			for (String each : scifi) {
-				System.out.println(each);
-				}
-		} else if (category.equals("comedy")) {
-			for (String each : comedy) {
-				System.out.println(each);
-				}
-		}
-		
-		checkout = MovieMethods.keepGoing(scan);
+
+			checkout = MovieMethods.keepGoing(scan);
 		} while (checkout == true);
 		System.out.println("Goodbye!");
-		
-	}		
-		
+
+	}
+
 }
-
-
-
